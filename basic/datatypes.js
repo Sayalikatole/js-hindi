6285d19dc9bf1de606cb3d27749cd63327de1781
Sayali Katole <124676789+Sayalikatole@users.noreply.github.com>
2024-12-12 04:58:59 +0000

    datatypes

---
+++ basic/datatypes.js
@@ -0,0 +1,18 @@
+"use strict"; //treat all js code as a newwer version
+
+let nmae ="sayali"
+let age = 13
+let isLoggin = true
+
+//number => 2 to power 53
+//bigint
+//string => ""
+//boolean => true/false
+//null => standalone value
+//undefined => 
+//symbol => unique
+//object
+
+console.log(typeof age)
+console.log(typeof null) //object
+console.log(typeof isLoggin)
---
+++ basic/variables.js
@@ -0,0 +1,16 @@
+console.log("hello worls")
+const accountId = 15445
+let accountName = "sayali" 
+var accountEmail = "sayali@123.com"
+var accountpass= "12344"
+accountCity = "akola"
+
+//in js let is use for block scope
+/* prefer not to use var because of issue of block scope and functinal scope*/ 
+//accountId=7566756   //not allowed
+accountName= "sysuy"
+accountEmail="saya.@122.com"
+accountpass="8785"
+console.log(accountName);
+console.table([accountEmail,accountId,accountName,accountCity])
+
