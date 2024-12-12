9733fcf73a7aba9477d0fdab0de3bb38f9ff0ba3
Sayali Katole <124676789+Sayalikatole@users.noreply.github.com>
2024-12-12 14:53:50 +0000

    string

--- basic/conversion.js
+++ basic/conversion.js
@@ -46,16 +46,21 @@ const name = "sayali"
 const repoCount = 30
 
 console.log(`Hello my name is ${name} and my repo count is ${repoCount}`)
-const gameName = new String ('sayali-tp')
+const gameName = new String ('sayali-tp-now-busy')
 console.log(gameName[0]);
 console.log(gameName.__proto__);
 console.log(gameName.charAt(4))
-console.log(gameName.toUpperCase());
-const newString = gameName.substring(0,4)
-console.log(newString) 
+// console.log(gameName.toUpperCase());
+// const newString = gameName.substring(0,4)
+// console.log(newString) 
 
+// const anotherString = gameName.slice(-8,4)
+// console.log(anotherString)
 
+const newString = "  sayali    " 
+console.log(newString.trim())
+console.log(newString)
+console.log(gameName.split('-'))
 
- 
 
 
