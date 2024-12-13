8b822fb21feb1234d0e1bc7c0ff25dd9b1c5fec0
Sayali Katole <124676789+Sayalikatole@users.noreply.github.com>
2024-12-13 12:27:36 +0000

    funct

---
+++ basic/functions.js
@@ -0,0 +1,14 @@
+// function addNum(num1,num2){
+//    console.log(num1 + num2) 
+
+// }
+function addNum(num1,num2){
+let result = num1+num2 
+return result
+ }
+const result = addNum(3,5)
+//console.log("result",result)
+function loginUser(username){
+    return `${username} just logged in`
+}
+console.log(loginUser("sayali"))
