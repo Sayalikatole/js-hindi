67c203aaa10029d02b80f7d80e26d4b36e8b219d
Sayali Katole <124676789+Sayalikatole@users.noreply.github.com>
2024-12-13 06:30:04 +0000

    date

---
+++ basic/number.js
@@ -0,0 +1,31 @@
+// const score = 400
+// const balance = new Number (100)
+// console.log(balance) 
+// const hundreds = 100000
+// console.log(typeof hundreds)
+// console.log(hundreds.toString())
+// console.log(hundreds.toLocaleString())
+
+//***************Maths*******************//
+console.log(Math)
+
+//Date in js 
+
+// let myDate = new Date();
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toISOString());
+// console.log(myDate.toLocaleDateString());
+// console.log(myDate.toLocaleString());
+
+// let myCreateDate =  new Date(2023,0,23,5,3)
+// console.log(myCreateDate)
+let myCreateDate = new Date("2023-01-14")
+ console.log(myCreateDate.toLocaleString())
+
+ let myTimestamp = Date.now()
+//  console.log(myTimestamp) //in millisceond 
+//  console.log(myCreateDate.getTime())
+//  console.log(Math.floor(Date.now()/1000))  // toconvert millsec into sec
+
+let newDate = new Date()
